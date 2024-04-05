94818becbee23f6c92c816d773ce5346eb461954
jonydav88 <jonatanmarquez88@gmail.com>
2024-04-05 17:18:14 -0300

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+/******for testing purposes****/
+IEnumerable<string> matrix = new List<string>()
+{
+    "tegtgo",
+    "goosgo",
+    "teasgo"
+};
+
+IEnumerable<string> wordStream = new List<string>()
+{
+    "go",
+    "tea"
+};
+/****************************/
+
+var wordFinder = new WordFinder(matrix);
+
+wordFinder.PrintResults(wordStream);
---
+++ ConsoleApp1/WordFinder.cs
@@ -0,0 +1,95 @@
+﻿namespace ConsoleApp1;
+
+public class WordFinder
+{
+    private IEnumerable<string> _matrix;
+
+    public WordFinder(IEnumerable<string> matrix)
+    {
+        _matrix = matrix;
+    }
+
+    public bool IsValidMatrix()
+    {
+        var wordLength = _matrix.First().Length;
+        var height = _matrix.Count();
+
+        return _matrix.All(x => x.Length <= 64) &&
+            _matrix.All(x => x.Length == wordLength) &&
+            height <= 64;
+    }
+
+    public Dictionary<string, int> Find(IEnumerable<string> wordStream)
+    {
+        Dictionary<string, int> foundWords = new Dictionary<string, int>();
+
+        IEnumerable<string> verticalMatrix = GetVerticalMatrix();
+
+        foreach (var word in wordStream)
+        {
+            var horizontalContainer = _matrix.Select(x => (x, x.Split(word).Count() - 1));
+            var verticalContainer = (verticalMatrix.Select(x => (x, x.Split(word).Count() - 1)));
+
+            BuildDictionary(ref foundWords, horizontalContainer, word);
+            BuildDictionary(ref foundWords, verticalContainer, word);
+        }
+
+        return foundWords;
+    }
+
+    public void PrintResults(IEnumerable<string> wordStream)
+    {
+        if (!IsValidMatrix())
+        {
+            Console.WriteLine("Verify the words' length. " +
+                "The length should be the same for each word " +
+                "and matrix should not be greater than 64x64");
+        }
+        else
+        {
+            var results = Find(wordStream)
+                .OrderByDescending(x => x.Value)
+                .Where(x => x.Value > 0);
+
+            foreach (var result in results)
+            {
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("word: " + result.Key + " Repetitions: " + result.Value);
+            }
+        }
+    }
+
+    private void BuildDictionary(ref Dictionary<string, int> foundWords,
+        IEnumerable<(string x, int)> contains,
+        string word)
+    {
+        if (contains.Any())
+        {
+            if (foundWords.ContainsKey(word))
+            {
+                foundWords[word] += contains.Sum(x => x.Item2);
+            }
+            else
+            {
+                foundWords[word] = contains.Sum(x => x.Item2);
+            }
+        }
+    }
+
+    private IEnumerable<string> GetVerticalMatrix()
+    {
+        var verticalMatrix = new List<string>();
+
+        var wordLenght = _matrix.First().Length;
+
+        for (var i = 0; i < wordLenght; i++)
+        {
+            var word = new string(_matrix
+                .Select(x => x.ToCharArray()[i]).ToArray());
+
+            verticalMatrix.Add(word);
+        }
+
+        return verticalMatrix;
+    }
+}
